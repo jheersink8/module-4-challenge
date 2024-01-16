add201048ba57f534a20fa50c8cf6b5764161c9c
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-01-15 21:24:58 -0700

    Minor aesthetic tweaks. This is a great fall-back point.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,9 @@
 var beginQuiz = document.getElementById("begin");
 var highScore = document.getElementById("highScore");
+var questionNumber = document.getElementById("questionNumber");
+var question = document.getElementById("question");
 
+
+questionNumber.textContent = "Question 3"
+
+question.textContent = "What is you favorite color?"
