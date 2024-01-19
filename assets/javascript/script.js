bc94fe6e205fe6956c0424e52329cb9daaa9bc93
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-01-19 00:15:56 -0700

    Streamlined code for answer output to buttons. Correct/Incorrect answers are defined in data-sets. Next step of current branch is confirming if button is correct/incorrect and also moving to next question.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,10 +1,10 @@
 var questionNumberOutput = document.querySelector(".questionNumberOutput");
 var questionOutput = document.querySelector(".questionOutput");
 var buttonChoice = document.querySelectorAll(".buttonChoice");
-var buttonA = document.getElementById("buttonA");
-var buttonB = document.getElementById("buttonB");
-var buttonC = document.getElementById("buttonC");
-var buttonD = document.getElementById("buttonD");
+var button1 = document.getElementById("button1");
+var button2 = document.getElementById("button2");
+var button3 = document.getElementById("button3");
+var button4 = document.getElementById("button4");
 var questionList = {
     questionNumber: ["Question 1", "Question 2", "Question 3", "Question 4", "Question 5", "Question 6", "Question 7", "Question 8", "Question 9", "Question 10"],
     question: ["Which of the following is not a proper variable in JavaScript?", "A for loop commonly contains all of the following except:", "What is the starting index point of an array?", "The __________ method will remove the white space from either side of a string.", "The __________ method will add an element to the end of an array.", "Which method would best be used to combine the values of two arrays into one array?", "Console.log(document.body) will return:", "The setAttribute method is used to:", "Which event listener listens for a key being pressed?", "The textContent property is used to:"],
@@ -14,39 +14,29 @@ var questionList = {
     incorrectAnswer3: ["var those;", "i++", "It is defined by the user", "split", "split", "unshift", "All saved local storage values", "Reduce the number of JS variables ", "Change", "Make sites more accessible for screen readers"]
 }
 
-presentQuestion(9)
+presentQuestion(0)
 function presentQuestion(x) {
     // Present the question number and question//
     questionNumberOutput.textContent = questionList.questionNumber[x];
     questionOutput.textContent = questionList.question[x];
 
-    // Pull a random array value of "correctAnswer", "incorrectAnswer1", incorrectAnswer2", or "incorrectAnswer3" out of answerArrayMod0 three times. Each new variable will determine the order of the answers so that test takers can't predict their position.//
+    // Create a new array with "correctAnswer", "incorrectAnswer1", incorrectAnswer2", or "incorrectAnswer3" in a random order.//
     var answerArrayMod0 = ["correctAnswer", "incorrectAnswer1", "incorrectAnswer2", "incorrectAnswer3"];
+    var rndArray = [];
+    for (var i = answerArrayMod0.length; i > 0; i--) {
+        var removeArray = answerArrayMod0.splice((Math.floor(Math.random() * i)), 1);
+        rndArray.push(removeArray[0]);
+    };
 
-
-    var answerArrayMod1 = answerArrayMod0.splice((Math.floor(Math.random() * answerArrayMod0.length)), 1);
-    var answerArrayMod2 = answerArrayMod0.splice((Math.floor(Math.random() * answerArrayMod0.length)), 1);
-    var answerArrayMod3 = answerArrayMod0.splice((Math.floor(Math.random() * answerArrayMod0.length)), 1);
-
-    // Assign that random value to the constant button positions. This prevents the question buttons from ever being in the same position.// 
-    buttonA.textContent = questionList[answerArrayMod0.toString()][x];
-    buttonB.textContent = questionList[answerArrayMod1.toString()][x];
-    buttonC.textContent = questionList[answerArrayMod2.toString()][x];
-    buttonD.textContent = questionList[answerArrayMod3.toString()][x];
-
-    // buttonA.setAttribute("data-answer")
-
-
-
+    // Place values in buttons and also assign data-sets on if the button is correct or incorrect//
+    for (var j = 0; j < rndArray.length; j++) {
+        if (rndArray[j] === "correctAnswer") {
+            buttonChoice[j].textContent = questionList[rndArray[j].toString()][x];
+            buttonChoice[j].setAttribute("data-answer", "correctAnswer")
+        } else {
+            buttonChoice[j].textContent = questionList[rndArray[j].toString()][x];
+            buttonChoice[j].setAttribute("data-answer", "incorrectAnswer")
+        }
+    }
 };
 
-
-
-
-
-
-// function answerQuestion() {
-
-// }
-
-// buttonA.addEventListener("click", "answerQuestion")
