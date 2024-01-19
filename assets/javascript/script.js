95e2bf8ae2aa8ad72c817459f4c2a7b8cb3616e3
jheersink8 <150620321+jheersink8@users.noreply.github.com>
2024-01-18 22:10:13 -0700

    Button random working. Working on cleaning code.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,10 @@
+var questionNumberOutput = document.querySelector(".questionNumberOutput");
+var questionOutput = document.querySelector(".questionOutput");
+var buttonChoice = document.querySelectorAll(".buttonChoice");
+var buttonA = document.getElementById("buttonA");
+var buttonB = document.getElementById("buttonB");
+var buttonC = document.getElementById("buttonC");
+var buttonD = document.getElementById("buttonD");
 var questionList = {
     questionNumber: ["Question 1", "Question 2", "Question 3", "Question 4", "Question 5", "Question 6", "Question 7", "Question 8", "Question 9", "Question 10"],
     question: ["Which of the following is not a proper variable in JavaScript?", "A for loop commonly contains all of the following except:", "What is the starting index point of an array?", "The __________ method will remove the white space from either side of a string.", "The __________ method will add an element to the end of an array.", "Which method would best be used to combine the values of two arrays into one array?", "Console.log(document.body) will return:", "The setAttribute method is used to:", "Which event listener listens for a key being pressed?", "The textContent property is used to:"],
@@ -7,21 +14,16 @@ var questionList = {
     incorrectAnswer3: ["var those;", "i++", "It is defined by the user", "split", "split", "unshift", "All saved local storage values", "Reduce the number of JS variables ", "Change", "Make sites more accessible for screen readers"]
 }
 
-var questionNumberOutput = document.querySelector(".questionNumberOutput");
-var questionOutput = document.querySelector(".questionOutput");
-var buttonA = document.getElementById("buttonA");
-var buttonB = document.getElementById("buttonB");
-var buttonC = document.getElementById("buttonC");
-var buttonD = document.getElementById("buttonD");
-
 presentQuestion(9)
 function presentQuestion(x) {
     // Present the question number and question//
     questionNumberOutput.textContent = questionList.questionNumber[x];
     questionOutput.textContent = questionList.question[x];
-    
-    // Pull a random value out of each object position and assign it a constant value//
+
+    // Pull a random array value of "correctAnswer", "incorrectAnswer1", incorrectAnswer2", or "incorrectAnswer3" out of answerArrayMod0 three times. Each new variable will determine the order of the answers so that test takers can't predict their position.//
     var answerArrayMod0 = ["correctAnswer", "incorrectAnswer1", "incorrectAnswer2", "incorrectAnswer3"];
+
+
     var answerArrayMod1 = answerArrayMod0.splice((Math.floor(Math.random() * answerArrayMod0.length)), 1);
     var answerArrayMod2 = answerArrayMod0.splice((Math.floor(Math.random() * answerArrayMod0.length)), 1);
     var answerArrayMod3 = answerArrayMod0.splice((Math.floor(Math.random() * answerArrayMod0.length)), 1);
@@ -31,4 +33,20 @@ function presentQuestion(x) {
     buttonB.textContent = questionList[answerArrayMod1.toString()][x];
     buttonC.textContent = questionList[answerArrayMod2.toString()][x];
     buttonD.textContent = questionList[answerArrayMod3.toString()][x];
+
+    // buttonA.setAttribute("data-answer")
+
+
+
 };
+
+
+
+
+
+
+// function answerQuestion() {
+
+// }
+
+// buttonA.addEventListener("click", "answerQuestion")
